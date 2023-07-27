61fd9f2574e1913c84b619d4ee22cfd15ad0adb5
dheerajgajula02 <dheerajgajula2202@gmail.com>
2023-07-27 19:15:12 +0530

    another change

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+    print(name, email, message)
+
+    return render_template("submit.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
